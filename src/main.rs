97d5b75f1627792435dfcd6bf0e308448e5d8593
decatile <ohmycatile@gmail.com>
2025-04-15 16:58:39 +0300

    history repl

--- src/main.rs
+++ src/main.rs
@@ -3,32 +3,35 @@
 use lang::{Context, Func, Span};
 use nom::{Err, Input, Offset};
 use std::{
-    cell::UnsafeCell,
+    cell::{RefCell, UnsafeCell},
     io::{Write, stdin, stdout},
     iter::repeat_n,
+    rc::Rc,
 };
 
 mod lang;
 
 fn main() {
-    let mut buf = UnsafeCell::new(String::new());
-    let mut buf_read = 0;
+    let mut buf: Vec<UnsafeCell<String>> = vec![];
     let mut ctx = Context::new();
     loop {
         print!("> ");
         stdout().flush().unwrap();
-        let new_buf_read = stdin().read_line(buf.get_mut()).unwrap();
-        let span = Span::new(unsafe { &*buf.get() }).take_from(buf_read);
+        // SAFETY
+        // Раст не всегда понимает как используются данные
+        // В этом случае, мы используем не всю структуру строки для Span-ов,
+        // А лишь поинтер на хипе. Если мы муваем строку, хипа очевидно не изменится,
+        // А указатели не инвалидируются. Поэтому всё ок. (Я не придумал ничего лучше).
+        let mut string = UnsafeCell::new(String::new());
+        stdin().read_line(string.get_mut()).unwrap();
+        let span = Span::new(unsafe { &mut *string.get() }.as_str());
         match lang::program(span) {
             Ok((_, program)) => {
-                buf_read += new_buf_read;
                 match program {
-                    lang::Program::Expression(token) => {
-                        match ctx.evaluate_expression(&token) {
-                            Ok(result) => println!("{result}"),
-                            Err(err) => println!("{err:?}"),
-                        }
-                    }
+                    lang::Program::Expression(token) => match ctx.evaluate_expression(&token) {
+                        Ok(result) => println!("{result}"),
+                        Err(err) => println!("{err:?}"),
+                    },
                     lang::Program::Func(token) => {
                         ctx.funcs
                             .insert(token.data.ident.data.0.clone(), Func::Custom(token));
@@ -43,20 +46,19 @@ fn main() {
                             println!("{err:?}");
                         }
                     },
-                }
+                };
+                buf.push(string);
             }
-            Err(err) => {
-                match err {
-                    Err::Incomplete(needed) => println!("{needed:?}"),
-                    Err::Error(err) | Err::Failure(err) => {
-                        println!(
-                            "{}^- {}",
-                            repeat_n(' ', span.offset(&err.input) + 2).collect::<String>(),
-                            err.message,
-                        );
-                    }
+            Err(err) => match err {
+                Err::Incomplete(needed) => println!("{needed:?}"),
+                Err::Error(err) | Err::Failure(err) => {
+                    println!(
+                        "{}^- {}",
+                        repeat_n(' ', span.offset(&err.input) + 2).collect::<String>(),
+                        err.message,
+                    );
                 }
-            }
+            },
         }
     }
 }
